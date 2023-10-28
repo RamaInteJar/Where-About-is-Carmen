230faaa81578f68bb79183af9a21c5e050982c2c
pentiumKing <ramahawai87@gmail.com>
2023-10-27 21:57:49 -0400

    added a comment

--- answers.sql
+++ answers.sql
@@ -23,6 +23,7 @@ SELECT * FROM country LIMIT 1;
 --Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
 -- ANSWER:
 neondb=> SELECT *FROM country WHERE region = 'Southern Europe' ORDER BY population ASC LIMIT 1;
+-- ANSWER:
  code |             name              | continent |     region      | surfacearea | indepyear | population | lifeexpectancy | gnp  | gnpold |            localname            |      governmentform      |     headofstate     | capital | code2 
 ------+-------------------------------+-----------+-----------------+-------------+-----------+------------+----------------+------+--------+---------------------------------+--------------------------+---------------------+---------+-------
  VAT  | Holy See (Vatican City State) | Europe    | Southern Europe |         0.4 |      1929 |       1000 |                | 9.00 |        | Santa Sede/Cittï¿½ del Vaticano | Independent Church State | Johannes Paavali II |    3538 | VA
