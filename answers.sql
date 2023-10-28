aa96626bcdb2d45c8d5faec96a372feb4e9469cd
pentiumKing <ramahawai87@gmail.com>
2023-10-27 22:43:23 -0400

    final answer

--- answers.sql
+++ answers.sql
@@ -71,5 +71,32 @@ SELECT * FROM country WHERE code='BRA';
 (1 row)
 
 --clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
+neondb=> SELECT * FROM city WHERE id='211';
+-- ANSWER:
+ id  |    name    | countrycode |     district     | population 
+-----+------------+-------------+------------------+------------
+ 211 | Brasï¿½lia | BRA         | Distrito Federal |    1969868
+(1 row)
 
+--clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock.
+-- Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
+-- Our playdate of late has been unusually fun –
+-- As an agent, I'll say, you've been a joy to outrun.
+-- And while the food here is great, and the people – so nice!
+-- I need a little more sunshine with my slice of life.
+-- So I'm off to add one to the population I find
+-- In a city of ninety-one thousand and now, eighty five.
+-- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
+-- ANSWER:
+neondb=> SELECT * FROM city WHERE population='91084';
+-- ANSWER:
+  id  |     name     | countrycode |  district  | population 
+------+--------------+-------------+------------+------------
+ 4060 | Santa Monica | USA         | California |      91084
+(1 row)
+
+-- ANSWER:
+--She is headed to Santa Monica, California, USA!
+
+ 
